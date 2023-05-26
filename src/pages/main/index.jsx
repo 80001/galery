0b198479652cc85954b9b64cb442665f2a92b1f1
80001 <romik.herman@gmail.com>
2023-05-26 21:26:17 +0300

    08. Edit modalPages: ZoomImage and CreatePost Add unsplashApi utils(get image by id)

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Gallery from '../gallery'
 import Blog from '../blog'
 import Account from '../account'
 import CreatePost from '../../components/modal/CreatePost'
-import ZoomModal from '../../components/modal/ZoomModal'
+//import ZoomModal from '../../components/modal/ZoomModal'
+import GalleryImage from '../gallery/image'
 
 const MainPage = () => {
+    let location = useLocation();
+
+    React.useEffect(() => {
+        console.log(location)
+    }, [location]);
     return (
         <div className='container'>
             <Routes>
                 <Route path='/' element={<Gallery />}>
-                    <Route path='image/*' element={<Gallery />} />
+                    <Route path='create_post/:id' element={<CreatePost />} />
+                    <Route path='image/:id' component={<GalleryImage />} />
                 </Route>
-                <Route path='create_post/*' element={<CreatePost />} />
-                <Route path='/blog' element={<Blog />}>
-                </Route>
-                <Route path='/account' element={<Account />}>
+                <Route path='blog' element={<Blog />} />
+                <Route path='account' element={<Account />}>
                 </Route>
             </Routes>
         </div>
