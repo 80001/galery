2125b35b8829a9bc984b18304120ff2bbaa5a491
80001 <romik.herman@gmail.com>
2023-07-03 18:20:31 +0300

    delete trash

--- src/store/account/account.selector.js
+++
@@ -1,2 +0,0 @@
-export const selectShowPosts = (state) => state.account.posts
-export const selectShowComments = (state) => state.account.comments
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -3,12 +3,10 @@ import { UserReducer } from "./user/user.reducer";
 import { BlogReducer } from "./blog/blog.reducer";
 import { SearchReducer } from "./search/search.reducer";
 import { ModalReducer } from "./modals/modals.reducer";
-import { AccountReducer } from "./account/account.reducer";
 
 export const rootReducer = combineReducers({
     user: UserReducer,
     blog: BlogReducer,
     search: SearchReducer,
     modal: ModalReducer,
-    account: AccountReducer,
 })
